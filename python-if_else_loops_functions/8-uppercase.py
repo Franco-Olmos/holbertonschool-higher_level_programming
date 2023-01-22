44c2aaf90cb84b79f7f59c7cd30338a954b97a96
Franco-Olmos <5749@holbertonstudents.com>
2023-01-22 04:25:57 -0800

    t7 pep8 fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -5,5 +5,5 @@ def uppercase(str):
         if (ord(c) > 96 and ord(c) > 123):
             is_upper += chr(ord(c)-32)
         else:
-            newtext+= c
+            is_upper += c
     print('{}'.format(is_upper))
