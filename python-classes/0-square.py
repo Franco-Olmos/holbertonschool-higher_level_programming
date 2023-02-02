a5664a0723c9769b454040465a8b0687a03cee4d
Franco-Olmos <5749@holbertonstudents.com>
2023-02-02 05:42:40 -0800

    doc. t1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ Write an empty class """
+
+
 class Square:
     """Empty class"""
     def __init__(self):
--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" Write a class that defines a square """
+
+
 class Square:
+    """ Class """
     def __init__(self, size):
+        """ Init """
         self.__size = size
