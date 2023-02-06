57289cb282b6c606c18db3e05386ca7bd1ce6ae3
Franco-Olmos <5749@holbertonstudents.com>
2023-02-05 16:58:47 -0800

    t3.4

---
+++ python-test_driven_development/4-print_square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that prints a square with #"""
+
+
+def print_square(size):
+    """print square"""
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if size == 0:
+        return
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+        print("")
