53977c4473614468ca8d3bde59370b7872134f32
Franco-Olmos <5749@holbertonstudents.com>
2023-03-17 08:17:29 -0700

    t0-t9

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+-- create a table in the current database
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+)
---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Display the numbers of records whit id int the table
+SELECT count(*) FROM first_table WHERE id = 89
