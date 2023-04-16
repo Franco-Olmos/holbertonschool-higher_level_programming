39fb79e3e5ee1f82dbf1ba3c63e49cd34d49b5f7
Franco-Olmos <5749@holbertonstudents.com>
2023-04-16 15:37:30 -0700

    t4 fix

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,31 +1,30 @@
 #!/usr/bin/node
 
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if ((w > 0) && (h > 0)) {
-      this.width = w;
-      this.height = h;
+class Rectangle {
+  constructor(w, h) {
+    if (w <= 0 || h <= 0) {
+      return {};
     }
-
-    Rectangle.prototype.print = function () {
-      for (let height = 0; height < h; height++) {
-        let printRectangle = '';
-        for (let width = 0; width < w; width++) {
-          printRectangle += 'X';
-        }
-        console.log(printRectangle);
-      }
-    };
-
-    Rectangle.prototype.double = function () {
-      this.height = this.height * 2;
-      this.width = this.width * 2;
-    };
-
-    Rectangle.prototype.rotate = function () {
-      let swap = this.width;
-      swap = this.height;
-      this.height = this.width;
-    };
+    this.width = w;
+    this.height = h;
   }
-};
+  
+  print() {
+    let row = '';
+    for (let i = 0; i < this.width; i++) {
+      row += 'X';
+    }
+    for (let j = 0; j < this.height; j++) {
+      console.log(row);
+    }
+  }
+  
+  rotate() {
+    [this.width, this.height] = [this.height, this.width];
+  }
+  
+  double() {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
