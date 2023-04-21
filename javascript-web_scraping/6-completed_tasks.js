937b39f4e24023d97e35fff57368d9d4fee27dca
Franco-Olmos <5749@holbertonstudents.com>
2023-04-21 08:07:27 -0700

    task6

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,25 @@
 #!/usr/bin/node
+
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, { json: true }, (err, response, data) => {
+  if (err) { 
+    console.log(err); 
+    return; 
+  }
+  const completedTasks = {};
+
+  data.forEach(task => {
+    if (task.completed === true) {
+      if (!completedTasks[task.userId]) {
+        completedTasks[task.userId] = 1;
+      } else {
+        completedTasks[task.userId]++;
+      }
+    }
+  });
+
+  console.log(completedTasks);
+});
