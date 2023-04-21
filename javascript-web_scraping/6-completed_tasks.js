73380f759d9fb42f8274d39a69f3cacff33517aa
Franco-Olmos <5749@holbertonstudents.com>
2023-04-21 08:18:52 -0700

    semistandard fix

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-
 const request = require('request');
 const url = process.argv[2];
 
 request(url, { json: true }, (err, response, data) => {
-  if (err) { 
-    console.log(err); 
-    return; 
+  if (err) {
+    console.log(err);
+    return;
   }
   const completedTasks = {};
 
